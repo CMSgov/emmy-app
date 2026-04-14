f0cda4657ad4a097d92214aaf646c6c5e7192c31
root <root@ip-169.254.173.10.ec2.internal>
2026-04-14 15:50:19 +0000

    Changes imported from DSACMS repository

--- app/app/components/activity_flow_progress_indicator.rb
+++ app/app/components/activity_flow_progress_indicator.rb
@@ -1,34 +1,62 @@
 class ActivityFlowProgressIndicator < ViewComponent::Base
-  def self.from_calculator(progress_calculator, agency_full_name, variant: :standard, required_month_count: nil)
+  def self.from_calculator(
+    progress_calculator,
+    variant: :application,
+    required_month_count: nil
+  )
     new(
       monthly_calculation_results: progress_calculator.monthly_results,
-      agency_full_name: agency_full_name,
       variant: variant,
       required_month_count: required_month_count
     )
   end
 
-  def initialize(monthly_calculation_results:, agency_full_name:, variant: :standard, required_month_count: nil)
+  def initialize(
+    monthly_calculation_results:,
+    variant: :application,
+    required_month_count: nil
+  )
     @monthly_calculation_results = monthly_calculation_results
-    @agency_full_name = agency_full_name
-    @renewal = variant.to_s == "renewal"
+    @renewal = variant == :renewal
     @required_month_count = normalize_required_month_count(required_month_count)
   end
 
   def percent_complete(monthly_result)
+    progress_value = progress_value_for(monthly_result)
+    threshold_value = completion_threshold_for(monthly_result)
+
     [
-      (100.0 * monthly_result.total_hours) / completion_threshold,
+      (100.0 * progress_value) / threshold_value,
       100
     ].min
   end
 
-  def completion_threshold = ActivityFlowProgressCalculator::PER_MONTH_HOURS_THRESHOLD
+  def hours_completion_threshold = ActivityFlowProgressCalculator::PER_MONTH_HOURS_THRESHOLD
+  def earnings_completion_threshold = ActivityFlowProgressCalculator::PER_MONTH_EARNINGS_THRESHOLD
 
   def format_hours(hours)
     return hours.to_i if hours.to_i == hours
 
     hours.round(1)
   end
+
+  def display_progress_amount(monthly_result)
+    if display_dollars?(monthly_result)
+      format_dollar_amount(monthly_result.total_earnings_cents)
+    else
+      format_hours(monthly_result.total_hours)
+    end
+  end
+
+  def display_completion_threshold(monthly_result)
+    if display_dollars?(monthly_result)
+      format_dollar_amount(earnings_completion_threshold)
+    else
+      hours_completion_threshold
+    end
+  end
+
+  def display_hours_unit?(monthly_result) = !display_dollars?(monthly_result)
 
   def multi_month? = monthly_calculation_results.length > 1
 
@@ -48,12 +76,38 @@ class ActivityFlowProgressIndicator < ViewComponent::Base
 
   def renewal? = @renewal
 
+  def renewal_requires_subset_months? = renewal? && required_month_count < total_month_count
+
   def reporting_window_start_month = ordered_monthly_calculation_results.first&.month
 
   def reporting_window_end_month = ordered_monthly_calculation_results.last&.month
 
   private
-  attr_reader :monthly_calculation_results, :agency_full_name, :required_month_count
+  attr_reader :monthly_calculation_results, :required_month_count
+
+  def display_dollars?(monthly_result)
+    monthly_result.default_unit == :dollars
+  end
+
+  def progress_value_for(monthly_result)
+    if display_dollars?(monthly_result)
+      monthly_result.total_earnings_cents.to_f
+    else
+      monthly_result.total_hours.to_f
+    end
+  end
+
+  def completion_threshold_for(monthly_result)
+    if display_dollars?(monthly_result)
+      earnings_completion_threshold
+    else
+      hours_completion_threshold
+    end
+  end
+
+  def format_dollar_amount(cents)
+    helpers.number_to_currency(cents.to_f / 100, precision: 0)
+  end
 
   def normalize_required_month_count(required_month_count)
     requested_count = required_month_count || monthly_calculation_results.length
